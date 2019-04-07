899fcb8552b441818a9c9fd78be0a2ec5b6622f0
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2019-04-06 18:10:46 -0700

    Issue #x Added test code to evaluate the grammar

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,10 +1,23 @@
 package compiler;
-import org.antlr.v4.runtime.*;
+import org.antlr.v4.*;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 
 public class Compiler {
     public static void main(String args[]) {
-        System.out.println("hello compiler ");
+        System.out.println("compilation started.. ");
+        // String chatContent = "aj SAYS: hello\n";
+        // ChatLexer chatLexer = new ChatLexer(CharStreams.fromString(chatContent));
+        // CommonTokenStream tokens = new CommonTokenStream(chatLexer);
+        // ChatParser parser = new ChatParser(tokens);
+        // ParseTree tree = parser.chat();
+        // System.out.println(tree);
+        // ParseTreeWalker walker = new ParseTreeWalker();
+        // ChatEvaluator listener= new ChatEvaluator();
+        // walker.walk(listener, tree);
 
     }
 }
