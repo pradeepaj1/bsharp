29bd10d412e2cfb6a59d906aecd45f7fab912100
Sneha Narasimhan <snehanarasimhan@ymail.com>
2019-04-23 15:12:09 -0700

    Compiler design altered to suit file stream reader.

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,5 +1,6 @@
 package compiler;
 
+
 import main.antlr4.generatecode.BSharpLexer;
 import main.antlr4.generatecode.BSharpParser;
 import org.antlr.v4.runtime.CharStreams;
@@ -11,20 +12,21 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 
+
 public class Compiler {
     public static void main(String args[]) {
         System.out.println("compilation started.. ");
 
-         BSharpLexer lexer = new BSharpLexer(CharStreams.fromString(readProgramFromFile()));
-         CommonTokenStream tokens = new CommonTokenStream(lexer);
-         BSharpParser parser = new BSharpParser(tokens);
-         ParseTree tree = ((BSharpParser) parser).writeStatement();
-         System.out.println(tree);
-         ParseTreeWalker walker = new ParseTreeWalker();
-         BSharpListener listener= new BSharpListener();
-         walker.walk(listener, tree);
-
+        BSharpLexer lexer = new BSharpLexer(CharStreams.fromString(readProgramFromFile()));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        BSharpParser parser = new BSharpParser(tokens);
+        ParseTree tree = parser.bSharp();
+        System.out.println(tree);
+        ParseTreeWalker walker = new ParseTreeWalker();
+        BSharpListener listener= new BSharpListener();
+        walker.walk(listener, tree);
     }
+
     private static String readProgramFromFile() {
         String program = null;
         try {
