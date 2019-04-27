d8bcf97d2ece97d876d97757a5b248b99b2b1583
hkolli <hkolli@asu.edu>
2019-04-27 16:17:00 -0700

    Updated the path of intermediate code in runner.

--- src/runtime/Runner.java
+++ src/runtime/Runner.java
@@ -40,13 +40,13 @@ public class Runner {
         if (args.length > 0) {
             intermediateCodeFilePath = args[0];
         } else {
-            intermediateCodeFilePath = "data/DefaultProgram/defaultprogram.bsharp";
+            intermediateCodeFilePath = "data/DefaultProgram/defaultprogram.bsharp.intermediate";
         }
         run(intermediateCodeFilePath);
     }
 
     private static void run(String intermediateCodeFilePath) {
-        code = readProgramFromFile(intermediateCodeFilePath+".intermediate");
+        code = readProgramFromFile(intermediateCodeFilePath);
         System.out.println("\n\n******** PROGRAM OUTPUT ********");
         while (programCounter < code.size()) {
             evaluateLine(code.get(programCounter));
